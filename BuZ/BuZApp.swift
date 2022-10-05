99f56e31093cfa9bd40d4707c1ce0c3d3e47884f
caroli <eduardodovalecaroli@gmail.com>
2022-10-05 18:20:05 -0300

    Initial Commit

---
+++ BuZ/BuZApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuZApp.swift
+//  BuZ
+//
+//  Created by Eduardo do Vale Caroli on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BuZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
