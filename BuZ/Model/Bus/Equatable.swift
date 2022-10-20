17cc8d407195fbf1e0ae162e32ad502fa87d49bc
caroli <eduardodovalecaroli@gmail.com>
2022-10-20 17:39:49 -0300

    Bus tem Equatable, Função busesChanged
    Criei uma extensão Equatable para a classe Bus, e uma função busesChanged, para verificar se os novos ônibus recebidos de uma requisição são fruto de uma nova leitura. Apenas se forem eu irei fazer request do ETA.

---
+++ BuZ/Model/Bus/Equatable.swift
@@ -0,0 +1,8 @@
+//
+//  Equatable.swift
+//  BuZ
+//
+//  Created by Eduardo do Vale Caroli on 20/10/22.
+//
+
+import Foundation
---
+++ BuZ/Services/BusLocation/BusesHaveChanged.swift
@@ -0,0 +1,8 @@
+//
+//  BusesHaveChanged.swift
+//  BuZ
+//
+//  Created by Eduardo do Vale Caroli on 20/10/22.
+//
+
+import Foundation
